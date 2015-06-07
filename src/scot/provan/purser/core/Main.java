bf723aee74c3084fb6250ca1f3d27ed4586ad423
Mark Provan <mrp4@st-andrews.ac.uk>
2015-06-07 21:09:29 +0100

    Added initial main methods for program and testing.

---
+++ src/scot/provan/purser/core/Main.java
@@ -0,0 +1,10 @@
+package scot.provan.purser.core;
+
+/**
+ * Created by Mark on 07/06/2015.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/scot/provan/purser/testing/TestMain.java
@@ -0,0 +1,10 @@
+package scot.provan.purser.testing;
+
+/**
+ * Created by Mark on 07/06/2015.
+ */
+public class TestMain {
+    public static void main(String[] args) {
+        System.out.println("Testing one two three...");
+    }
+}
