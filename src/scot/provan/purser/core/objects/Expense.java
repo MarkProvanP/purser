8562e27d3bd692444076048c7f5c9ce9ab63fa02
Mark Provan <mrp4@st-andrews.ac.uk>
2015-06-11 20:41:47 +0100

    Remove magic numbers in Income/Expense constructor
    Put the maximum and minimum transaction amounts in constants for time
    being. Will be changed in future to allow different maximum and minimum
    amounts per User or Project.

--- src/scot/provan/purser/core/objects/Expense.java
+++ src/scot/provan/purser/core/objects/Expense.java
@@ -9,9 +9,15 @@ import java.util.UUID;
  * Created by Mark on 07/06/2015.
  */
 public class Expense extends Transaction {
+    // TODO implement variable transaction min/max amount - per user/project?
+    public static final double EXPENSE_MIN_AMOUNT = Double.MIN_VALUE;
+    public static final double EXPENSE_MAX_AMOUNT = 0.0;
+
     public Expense(TransactionDataBundle bundle, UUID addedBy, Organisation org)
             throws TransactionAmountException, PurserObjectNotFoundException {
         super(bundle, addedBy, org);
-        if (bundle.getAmount() >= 0) throw new TransactionAmountException(bundle, Double.MIN_VALUE, 0, addedBy, org);
+        if (bundle.getAmount() >= 0) {
+            throw new TransactionAmountException(bundle, EXPENSE_MIN_AMOUNT, EXPENSE_MAX_AMOUNT, addedBy, org);
+        }
     }
 }
--- src/scot/provan/purser/core/objects/Income.java
+++ src/scot/provan/purser/core/objects/Income.java
@@ -10,10 +10,15 @@ import java.util.UUID;
  * Created by Mark on 07/06/2015.
  */
 public class Income extends Transaction {
+    // TODO implement variable transaction min/max amount - per user/project?
+    public static final double INCOME_MIN_AMOUNT = 0.0;
+    public static final double INCOME_MAX_AMOUNT = Double.MAX_VALUE;
 
     public Income(TransactionDataBundle bundle, UUID addedBy, Organisation org)
             throws TransactionCreationException, PurserObjectNotFoundException {
         super(bundle, addedBy, org);
-        if (bundle.getAmount() <= 0) throw new TransactionAmountException(bundle, 0, Double.MAX_VALUE, addedBy, org);
+        if (bundle.getAmount() <= 0) {
+            throw new TransactionAmountException(bundle, INCOME_MIN_AMOUNT, INCOME_MAX_AMOUNT, addedBy, org);
+        }
     }
 }
